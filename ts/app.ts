95aad17b921986840287f018ea0fc9183b9e9bd4
Ali Shazly <alishazly302@gmail.com>
2022-04-07 15:00:18 -0700

    Add loading text & sample data fetching

--- ts/app.ts
+++ ts/app.ts
@@ -11,11 +11,29 @@ export interface TrackData {
 
 initDropArea((tracks, extTracks) => {
     document.getElementById('input-selection').style.display = 'none';
-    drawGraph(tracks, extTracks);
+    const loadingText = document.getElementById('loading-text');
+    loadingText.style.display = 'inline';
+    setTimeout(() => {
+        drawGraph(tracks, extTracks);
+        loadingText.style.display = 'none';
+    }, 0);
 });
 
 document.getElementById('sample-button').addEventListener('click', () => {
-    document.getElementById('input-selection').style.display = 'none';
-    // read local storage for sample data
-    // drawGraph(tracks,extTracks);
+    const loadingText = document.getElementById('loading-text');
+    const inputSelection = document.getElementById('input-selection');
+    fetch('sample.json')
+        .then((res) => {
+            if (res.ok) {
+                inputSelection.style.display = 'none';
+                loadingText.style.display = 'inline';
+                return res.json();
+            } else {
+                return Promise.reject();
+            }
+        })
+        .then((extTracks) => {
+            drawGraph([], extTracks);
+            loadingText.style.display = 'none';
+        });
 });
